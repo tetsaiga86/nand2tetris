efd694b5307370053281044fa6c97efa5bb1194a
kyle <tetsaiga86@gmail.com>
2017-11-11 21:56:30 -0800

    compilationEngine passing 6/7 tests, failing to build SquareT.xml

--- projects/10/SyntaxAnalyzer/analyzer.js
+++ projects/10/SyntaxAnalyzer/analyzer.js
@@ -4,4 +4,13 @@ var compile = require('./compilationEngine.js');
 // tokenize('../ArrayTest');
 // tokenize('../ExpressionLessSquare');
 // tokenize('../Square');
-compile('./myOutputFiles/ArrayTest/MainT.xml')
+
+// compile('./myOutputFiles/ArrayTest/MainT.xml')
+
+// compile('./myOutputFiles/ExpressionLessSquare/MainT.xml')
+// compile('./myOutputFiles/ExpressionLessSquare/SquareGameT.xml')
+// compile('./myOutputFiles/ExpressionLessSquare/SquareT.xml')
+
+// compile('./myOutputFiles/Square/MainT.xml')
+// compile('./myOutputFiles/Square/SquareGameT.xml')
+compile('./myOutputFiles/Square/SquareT.xml')
